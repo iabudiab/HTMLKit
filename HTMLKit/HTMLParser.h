721839f54a861947fc18380d9e6983db637b695d
iska <iska.gp@gmail.com>
2014-10-04 21:58:25 +0200

    Add class stub for HTML Parser

---
+++ HTMLKit/HTMLParser.h
@@ -0,0 +1,13 @@
+//
+//  HTMLParser.h
+//  HTMLKit
+//
+//  Created by Iska on 04/10/14.
+//  Copyright (c) 2014 BrainCookie. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HTMLParser : NSObject
+
+@end
---
+++ HTMLKit/HTMLParser.m
@@ -0,0 +1,13 @@
+//
+//  HTMLParser.m
+//  HTMLKit
+//
+//  Created by Iska on 04/10/14.
+//  Copyright (c) 2014 BrainCookie. All rights reserved.
+//
+
+#import "HTMLParser.h"
+
+@implementation HTMLParser
+
+@end
