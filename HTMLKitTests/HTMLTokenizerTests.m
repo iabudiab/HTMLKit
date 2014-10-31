aaec5971b8c55c20695281f6284868b8019a29ea
iska <iska.gp@gmail.com>
2014-10-31 18:06:59 +0100

    Add performance test for tokenizing step

--- HTMLKitTests/HTMLTokenizerTests.m
+++ HTMLKitTests/HTMLTokenizerTests.m
@@ -49,8 +49,20 @@
 
 - (void)testPerformanceExample {
     // This is an example of a performance test case.
+- (void)testTokenizerPerformance
+{
+	NSString *path = [[NSBundle bundleForClass:self.class] resourcePath];
+	path = [path stringByAppendingPathComponent:@"HTML Standard.html"];
+
+	NSString *string = [NSString stringWithContentsOfFile:path encoding:NSUTF8StringEncoding error:nil];
+
     [self measureBlock:^{
-        // Put the code you want to measure the time of here.
+		HTMLTokenizer *tokenizer = [[HTMLTokenizer alloc] initWithString:string];
+
+		id token = nil;
+		do {
+			token = [tokenizer nextToken];
+		} while (token != nil);
     }];
 }
 
