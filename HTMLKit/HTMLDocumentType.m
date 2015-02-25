5e9846b8ea0d6603384aad752ba05bb7618f38ac
iska <iska.gp@gmail.com>
2015-02-25 23:48:24 +0100

    Add HTML Document Type node
    https://dom.spec.whatwg.org/#interface-documenttype

---
+++ HTMLKit/HTMLDocumentType.m
@@ -0,0 +1,35 @@
+//
+//  HTMLDocumentType.m
+//  HTMLKit
+//
+//  Created by Iska on 25/02/15.
+//  Copyright (c) 2015 BrainCookie. All rights reserved.
+//
+
+#import "HTMLDocumentType.h"
+
+@implementation HTMLDocumentType
+
+- (instancetype)initWithName:(NSString *)name
+			publicIdentifier:(NSString *)publicIdentifier
+			systemIdentifier:(NSString *)systemIdentifier
+{
+	self = [super initWithName:name type:HTMLDocumentTypeNode];
+	if (self) {
+		_publicIdentifier = [publicIdentifier copy] ?: @"";
+		_systemIdentifier = [systemIdentifier copy] ?: @"";
+	}
+	return self;
+}
+
+#pragma mark - NSCopying
+
+- (id)copyWithZone:(NSZone *)zone
+{
+	HTMLDocumentType *copy = [super copyWithZone:zone];
+	copy->_publicIdentifier = self.publicIdentifier;
+	copy->_systemIdentifier = self.systemIdentifier;
+	return copy;
+}
+
+@end
