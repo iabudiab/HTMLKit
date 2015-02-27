79856255dae951021b333a9242b040aaf82c95db
iska <iska.gp@gmail.com>
2015-02-28 00:56:35 +0100

    Add header that imports all HTML Nodes for convenience

--- HTMLKit/HTMLNode.h
+++ HTMLKit/HTMLNode.h
@@ -43,9 +43,9 @@ typedef NS_ENUM(short, HTMLNodeType)
 
 @property (nonatomic, strong, readonly) NSOrderedSet *childNodes;
 
-@property (nonatomic, strong, readonly) HTMLNode *firstNode;
+@property (nonatomic, strong, readonly) HTMLNode *firstChiledNode;
 
-@property (nonatomic, strong, readonly) HTMLNode *lastNode;
+@property (nonatomic, strong, readonly) HTMLNode *lastChildNode;
 
 @property (nonatomic, strong, readonly) HTMLNode *previousSibling;
 
---
+++ HTMLKit/HTMLNodes.h
@@ -0,0 +1,14 @@
+//
+//  HTMLNodes.h
+//  HTMLKit
+//
+//  Created by Iska on 27/02/15.
+//  Copyright (c) 2015 BrainCookie. All rights reserved.
+//
+
+#import "HTMLNode.h"
+#import "HTMLDocument.h"
+#import "HTMLDocumentType.h"
+#import "HTMLElement.h"
+#import "HTMLComment.h"
+#import "HTMLText.h"
