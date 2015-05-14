3cb5ed9a420d651847bc48af0a6fba0fe07ae6ed
iska <iska.gp@gmail.com>
2015-05-14 16:32:30 +0200

    Add implementation for a CSS Attribute Selector
    http://www.w3.org/TR/css3-selectors/#attribute-selectors

---
+++ HTMLKit/CSSAttributeSelector.h
@@ -0,0 +1,33 @@
+//
+//  CSSAttributeSelector.h
+//  HTMLKit
+//
+//  Created by Iska on 14/05/15.
+//  Copyright (c) 2015 BrainCookie. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "CSSSelector.h"
+
+typedef NS_ENUM(NSUInteger, CSSAttributeSelectorType)
+{
+	CSSAttributeSelectorExists,
+	CSSAttributeSelectorExactMatch,
+	CSSAttributeSelectorIncludes,
+	CSSAttributeSelectorBegins,
+	CSSAttributeSelectorEnds,
+	CSSAttributeSelectorContains,
+	CSSAttributeSelectorHyphen
+};
+
+@interface CSSAttributeSelector : CSSSelector
+
+@property (nonatomic, assign) CSSAttributeSelectorType type;
+@property (nonatomic, copy) NSString *name;
+@property (nonatomic, copy) NSString *value;
+
+- (instancetype)initWithType:(CSSAttributeSelectorType)type
+			   attributeName:(NSString *)name
+			  attrbiuteValue:(NSString *)value;
+
+@end
---
+++ HTMLKit/CSSAttributeSelector.m
@@ -0,0 +1,89 @@
+//
+//  CSSAttributeSelector.m
+//  HTMLKit
+//
+//  Created by Iska on 14/05/15.
+//  Copyright (c) 2015 BrainCookie. All rights reserved.
+//
+
+#import "CSSAttributeSelector.h"
+#import "HTMLElement.h"
+#import "NSCharacterSet+HTMLKit.h"
+
+@interface CSSAttributeSelector ()
+{
+	CSSAttributeSelectorType _type;
+	NSString *_name;
+	NSString *_value;
+}
+@end
+
+@implementation CSSAttributeSelector
+
+- (instancetype)initWithType:(CSSAttributeSelectorType)type
+			   attributeName:(NSString *)name
+			  attrbiuteValue:(NSString *)value
+{
+	self = [super init];
+	if (self) {
+		self.type = type;
+		self.name = [name copy];
+		self.value = [value copy];
+	}
+	return self;
+}
+
+- (BOOL)matchesElement:(HTMLElement *)element
+{
+	switch (_type) {
+		case CSSAttributeSelectorExists:
+		{
+			return !!element[_name];
+		}
+		case CSSAttributeSelectorExactMatch:
+		{
+			return [element[_name] isEqualToString:_value];
+		}
+		case CSSAttributeSelectorIncludes:
+		{
+			NSArray *components = [element[_name] componentsSeparatedByCharactersInSet:[NSCharacterSet HTMLWhitespaceCharacterSet]];
+			return [components containsObject:_value];
+		}
+		case CSSAttributeSelectorBegins:
+		{
+			return [element[_name] hasPrefix:_value];
+		}
+		case CSSAttributeSelectorEnds:
+		{
+			return [element[_name] hasSuffix:_value];
+		}
+		case CSSAttributeSelectorContains:
+		{
+			return [element[_name] containsString:_value];
+		}
+		case CSSAttributeSelectorHyphen:
+		{
+			return [element[_name] isEqualToString:_value] || [element[_name] hasSuffix:[_value stringByAppendingString:@"-"]];
+		}
+		default:
+			return NO;
+	}
+}
+
+- (NSString *)description
+{
+	if (self.type == CSSAttributeSelectorExists) {
+		return [NSString stringWithFormat:@"<%@: %p '%@'>", self.class, self, self.name];
+	}
+
+	NSString *matcher = @{@(CSSAttributeSelectorExactMatch): @"=",
+						  @(CSSAttributeSelectorIncludes): @"~=",
+						  @(CSSAttributeSelectorBegins): @"^=",
+						  @(CSSAttributeSelectorEnds): @"$=",
+						  @(CSSAttributeSelectorContains): @"*=",
+						  @(CSSAttributeSelectorHyphen): @"|="}[@(self.type)];
+
+	return [NSString stringWithFormat:@"<%@: %p '%@' %@ '%@'>", self.class, self, self.name, matcher, self.value];
+}
+
+@end
