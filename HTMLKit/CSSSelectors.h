c401c3ca2c757279c31a6b4c261559694f70a92d
iska <iska.gp@gmail.com>
2015-05-14 17:09:17 +0200

    Add umbrella header for all CSS Selectors

---
+++ HTMLKit/CSSSelectors.h
@@ -0,0 +1,13 @@
+//
+//  CSSSelectors.h
+//  HTMLKit
+//
+//  Created by Iska on 14/05/15.
+//  Copyright (c) 2015 BrainCookie. All rights reserved.
+//
+
+#ifndef HTMLKit_CSSSelectors_h
+#define HTMLKit_CSSSelectors_h
+
+
+#endif
