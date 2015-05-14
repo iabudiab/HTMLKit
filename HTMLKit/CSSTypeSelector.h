acec99ffead77b874977bf5ef3062f9675c91387
iska <iska.gp@gmail.com>
2015-05-14 16:23:23 +0200

    Add implementation for a CSS Type Selector
    Matches an Element with the given tag name or all Elements (universal selector)
    http://www.w3.org/TR/css3-selectors/#type-selectors

---
+++ HTMLKit/CSSTypeSelector.h
@@ -0,0 +1,19 @@
+//
+//  CSSTypeSelector.h
+//  HTMLKit
+//
+//  Created by Iska on 13/05/15.
+//  Copyright (c) 2015 BrainCookie. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "CSSSelector.h"
+
+@interface CSSTypeSelector : CSSSelector
+
+@property (nonatomic, copy) NSString *type;
+
++ (instancetype)universalSelector;
+- (instancetype)initWithType:(NSString *)type;
+
+@end
---
+++ HTMLKit/CSSTypeSelector.m
@@ -0,0 +1,45 @@
+//
+//  CSSTypeSelector.m
+//  HTMLKit
+//
+//  Created by Iska on 13/05/15.
+//  Copyright (c) 2015 BrainCookie. All rights reserved.
+//
+
+#import "CSSTypeSelector.h"
+#import "HTMLElement.h"
+#import "NSString+HTMLKit.h"
+
+@interface CSSTypeSelector ()
+{
+	NSString *_type;
+}
+@end
+
+@implementation CSSTypeSelector
+
++ (instancetype)universalSelector
+{
+	return [[self alloc] initWithType:@"*"];
+}
+
+- (instancetype)initWithType:(NSString *)type
+{
+	self = [super init];
+	if (self) {
+		self.type = [type copy];
+	}
+	return self;
+}
+
+- (BOOL)matchesElement:(HTMLElement *)element
+{
+	return [_type isEqualToString:@"*"] || [_type isEqualToStringIgnoringCase:element.tagName];
+}
+
+- (NSString *)description
+{
+	return [NSString stringWithFormat:@"<%@: %p '%@'>", self.class, self, self.type];
+}
+
+@end
