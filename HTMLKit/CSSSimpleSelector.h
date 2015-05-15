4f03e00003d80391eee45f6cdc1ebe5db01a69f8
iska <iska.gp@gmail.com>
2015-05-15 02:28:03 +0200

    Add a protocol for CSS Simple Selectors
    Simple Selectors should conform to this protocol in order to be addable to the Simple Selector Sequence.

---
+++ HTMLKit/CSSSimpleSelector.h
@@ -0,0 +1,13 @@
+//
+//  CSSSimpleSelector.h
+//  HTMLKit
+//
+//  Created by Iska on 14/05/15.
+//  Copyright (c) 2015 BrainCookie. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol CSSSimpleSelector <NSObject>
+
+@end
