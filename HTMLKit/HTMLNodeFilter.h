f8ee4a38b4cc7d8b5a96e5a8d38891f16fdb147f
iska <iska.gp@gmail.com>
2015-05-27 00:59:10 +0200

    Add HTML Node Filter protocol

---
+++ HTMLKit/HTMLNodeFilter.h
@@ -0,0 +1,36 @@
+//
+//  HTMLNodeFilter.h
+//  HTMLKit
+//
+//  Created by Iska on 27/05/15.
+//  Copyright (c) 2015 BrainCookie. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+typedef NS_ENUM(unsigned short, HTMLNodeFilterValue)
+{
+	HTMLNodeFilterAccept = 1,
+	HTMLNodeFilterReject = 2,
+	HTMLNodeFilterSkip = 3
+} HTMLNodeFilterValue;
+
+typedef NS_ENUM(unsigned long, HTMLNodeFilterShowOptions)
+{
+	HTMLNodeFilterShowAll = 0xFFFFFFFF;
+	HTMLNodeFilterShowElement = 0x1;
+	HTMLNodeFilterShowText = 0x4;
+	HTMLNodeFilterShowProcessingInstruction = 0x40;
+	HTMLNodeFilterShowComment = 0x80;
+	HTMLNodeFilterShowDocument = 0x100;
+	HTMLNodeFilterShowDocumentType = 0x200;
+	HTMLNodeFilterShowDocumentFragment = 0x400;
+} HTMLNodeFilterShowOptions;
+
+@class HTMLNode;
+
+@protocol HTMLNodeFilter <NSObject>
+
+- (HTMLNodeFilterValue)acceptNode:(HTMLNode *)node;
+
+@end
