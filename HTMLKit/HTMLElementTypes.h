f4bd5420c0d841244fa54319b8a67ee7d43a7572
iska <iska.gp@gmail.com>
2015-12-20 17:27:13 +0100

    Add source documentation for the Parser classes

--- HTMLKit/HTMLElementTypes.h
+++ HTMLKit/HTMLElementTypes.h
@@ -6,6 +6,10 @@
 //  Copyright (c) 2015 BrainCookie. All rights reserved.
 //
 
+///------------------------------------------------------
+/// HTMLKit private header
+///------------------------------------------------------
+
 #import	"HTMLElement.h"
 #import "HTMLNamespaces.h"
 #import "NSString+HTMLKit.h"
