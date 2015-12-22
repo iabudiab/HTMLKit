409b5502ae0b9bc0a791faf974b6470a734b578e
iska <iska.gp@gmail.com>
2015-12-22 17:57:21 +0100

    Add "Intro" page for HTMLKit in the playground

---
+++ HTMLKit.playground/Pages/Intro.xcplaygroundpage/Contents.swift
@@ -0,0 +1,13 @@
+/*:
+# HTMLKit
+
+![Logo](HTMLKit.png)
+
+> An Objective-C kit for your everyday HTML needs.
+
+HTMLKit is a [WHATWG](https://html.spec.whatwg.org/multipage/) specification-compliant library for parsing and serializing HTML documents and document fragments for OSX and iOS. HTMLKit parses real-world HTML the same way modern web browsers would.
+
+> HTMLKit is available under the MIT license.
+****
+[Next](@next)
+*/
