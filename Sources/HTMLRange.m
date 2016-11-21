9fd5d34244518e7ac1548440f1a1bc04dc9b1670
iska <iska.gp@gmail.com>
2016-11-21 01:17:48 +0100

    Add HTML Range class stub

---
+++ Sources/HTMLRange.m
@@ -0,0 +1,13 @@
+//
+//  HTMLRange.m
+//  HTMLKit
+//
+//  Created by Iska on 20/11/16.
+//  Copyright © 2016 BrainCookie. All rights reserved.
+//
+
+#import "HTMLRange.h"
+
+@implementation HTMLRange
+
+@end
