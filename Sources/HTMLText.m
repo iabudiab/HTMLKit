fe46b106b204e588a58bd292a8cda06a205f2cae
iska <iska.gp@gmail.com>
2017-01-08 22:36:47 +0100

    Add implementation for HTML Text splitting

--- Sources/HTMLText.m
+++ Sources/HTMLText.m
@@ -10,6 +10,8 @@
 #import "HTMLElement.h"
 #import "NSString+HTMLKit.h"
 #import "HTMLCharacterData+Private.h"
+#import "HTMLKitDOMExceptions.h"
+#import "HTMLDocument+Private.h"
 
 @implementation HTMLText
 
@@ -26,6 +28,42 @@
 - (void)appendString:(NSString *)string
 {
 	[self appendData:string];
+}
+
+NS_INLINE void CheckValidOffset(HTMLNode *node, NSUInteger offset, NSString *cmd)
+{
+	if (offset > node.length) {
+		[NSException raise:HTMLKitIndexSizeError
+					format:@"%@: Index Size Error, invalid offset %lu for splitting text node %@.",
+		 cmd, (unsigned long)offset, node];
+	}
+}
+
+- (HTMLNode *)splitTextAtOffset:(NSUInteger)offset
+{
+	CheckValidOffset(self, offset, NSStringFromSelector(_cmd));
+
+	NSUInteger length = self.length;
+	NSUInteger count = length - offset;
+	NSRange range = NSMakeRange(offset, count);
+
+	NSString *newData = [self.data substringWithRange:range];
+	HTMLText *newNode = [[HTMLText alloc] initWithData:newData];
+	[self.ownerDocument adoptNode:newNode];
+
+	HTMLNode *parent = self.parentNode;
+	if (parent != nil) {
+		[parent insertNode:newNode beforeChildNode:self.nextSibling];
+		[self.ownerDocument didInsertNewTextNode:newNode intoParent:parent afterSplittingTextNode:self atOffset:offset];
+	}
+
+	[self deleteDataInRange:range];
+
+	if (parent != nil) {
+		[self.ownerDocument clampRangesAfterSplittingTextNode:self atOffset:offset];
+	}
+
+	return newNode;
 }
 
 #pragma mark - Serialization
--- Sources/include/HTMLText.h
+++ Sources/include/HTMLText.h
@@ -30,6 +30,8 @@ NS_ASSUME_NONNULL_BEGIN
  */
 - (void)appendString:(NSString *)string __attribute__((deprecated("Use `appendData:` instead.")));
 
+- (HTMLNode *)splitTextAtOffset:(NSUInteger)offset;
+
 @end
 
 NS_ASSUME_NONNULL_END
