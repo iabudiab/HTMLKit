c13abcb83369b5d20e6dc81903f6619e8a7bcae2
iska <iska.gp@gmail.com>
2018-06-01 17:18:23 +0200

    Add implementation for HTML Attributes sanitizing policy

--- Sources/HTMLAttributePolicy.h
+++ Sources/HTMLAttributePolicy.h
@@ -8,6 +8,18 @@
 
 #import <Foundation/Foundation.h>
 
+NS_ASSUME_NONNULL_BEGIN
+
+typedef NSString * _Nullable (^ SanitizeAttrbiuteValueBlock) (NSString * _Nullable value, NSString * key);
+
 @interface HTMLAttributePolicy : NSObject
 
++ (instancetype)identity;
++ (instancetype)rejectAll;
++ (instancetype)policyWithBlock:(SanitizeAttrbiuteValueBlock)block;
+
+- (NSString *)sanitizeValue:(NSString *)value forKey:(NSString *)key;
+
 @end
+
+NS_ASSUME_NONNULL_END
--- Sources/HTMLAttributePolicy.m
+++ Sources/HTMLAttributePolicy.m
@@ -8,6 +8,49 @@
 
 #import "HTMLAttributePolicy.h"
 
+@interface HTMLAttributePolicy()
+{
+	SanitizeAttrbiuteValueBlock _block;
+}
+@end
+
 @implementation HTMLAttributePolicy
+
++ (instancetype)identity
+{
+	return [self policyWithBlock:^NSString * _Nullable (NSString * _Nullable value, NSString * _Nonnull key) {
+		return value;
+	}];
+}
+
++ (instancetype)rejectAll
+{
+	return [self policyWithBlock:^NSString * _Nullable (NSString * _Nullable value, NSString * _Nonnull key) {
+		return nil;
+	}];
+}
+
++ (instancetype)policyWithBlock:(SanitizeAttrbiuteValueBlock)block
+{
+	HTMLAttributePolicy *policy = [[HTMLAttributePolicy alloc] initWithBlock:block];
+	return policy;
+}
+
+- (instancetype)initWithBlock:(SanitizeAttrbiuteValueBlock)block
+{
+	self = [super init];
+	if (self) {
+		_block = block;
+	}
+	return self;
+}
+
+- (NSString *)sanitizeValue:(NSString *)value forKey:(NSString *)key
+{
+	if (_block) {
+		return _block(value, key);
+	}
+	return value;
+}
 
 @end
