785ca48f522952bd784f87920a3f2bcafec2f057
iska <iska.gp@gmail.com>
2018-06-01 16:34:56 +0200

    Add implementation for HTML Element sanitizing policy

--- Sources/HTMLElementPolicy.h
+++ Sources/HTMLElementPolicy.h
@@ -7,7 +7,25 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "HTMLSanitizingPolicy.h"
+#import "HTMLOrderedDictionary.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+typedef NSString * _Nullable (^ SanitizeElementNameBlock) (NSString *);
+typedef HTMLOrderedDictionary * _Nullable (^ SanitizeElementAttributesBlock) (HTMLOrderedDictionary * _Nullable);
 
 @interface HTMLElementPolicy : NSObject
 
++ (instancetype)identity;
++ (instancetype)rejectAll;
++ (instancetype)policyWithNameBlock:(SanitizeElementNameBlock)nameBlock;
++ (instancetype)policyWithNameBlock:(SanitizeElementNameBlock)nameBlock
+					attributesBlock:(nullable SanitizeElementAttributesBlock)attributesBlock;
+
+- (NSString *)sanitizeName:(NSString *)name;
+- (HTMLOrderedDictionary *)sanitzeAttributes:(HTMLOrderedDictionary *)attributes;
+
 @end
+
+NS_ASSUME_NONNULL_END
--- Sources/HTMLElementPolicy.m
+++ Sources/HTMLElementPolicy.m
@@ -8,6 +8,66 @@
 
 #import "HTMLElementPolicy.h"
 
+@interface HTMLElementPolicy()
+{
+	SanitizeElementNameBlock _nameBlock;
+	SanitizeElementAttributesBlock _attributesBlock;
+}
+@end
+
 @implementation HTMLElementPolicy
+
++ (instancetype)identity
+{
+	return [self policyWithNameBlock:^NSString * _Nullable (NSString * _Nonnull name) {
+		return name;
+	}];
+}
+
++ (instancetype)rejectAll
+{
+	return [self policyWithNameBlock:^NSString *_Nullable (NSString * _Nonnull name) {
+		return nil;
+	}];
+}
+
++ (instancetype)policyWithNameBlock:(NSString * _Nonnull (^)(NSString * _Nonnull))nameBlock
+{
+	return [self policyWithNameBlock:nameBlock attributesBlock:nil];
+}
+
++ (instancetype)policyWithNameBlock:(SanitizeElementNameBlock)nameBlock
+					attributesBlock:(SanitizeElementAttributesBlock)attributesBlock
+{
+	HTMLElementPolicy *policy = [[HTMLElementPolicy alloc] initWithNameBlock:nameBlock attributesBlock:attributesBlock];
+	return policy;
+}
+
+- (instancetype)initWithNameBlock:(SanitizeElementNameBlock)nameBlock
+				  attributesBlock:(SanitizeElementAttributesBlock)attributesBlock
+{
+	self = [super init];
+	if (self) {
+		_nameBlock = nameBlock;
+		_attributesBlock = attributesBlock;
+	}
+	return self;
+}
+
+- (NSString *)sanitizeName:(NSString *)name
+{
+	if (_nameBlock) {
+		return _nameBlock(name);
+	}
+	return name;
+}
+
+- (HTMLOrderedDictionary *)sanitzeAttributes:(HTMLOrderedDictionary *)attributes
+{
+	if (_attributesBlock) {
+		return _attributesBlock(attributes);
+	}
+	return attributes;
+}
 
 @end
